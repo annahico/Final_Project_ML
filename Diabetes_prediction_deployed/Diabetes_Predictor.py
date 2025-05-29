fd081d1db0944c4d09f3e1eddc2dfa8cc9456f5d
annahico <annahico@gmail.com>
2025-05-29 11:25:14 +0200

    feat: implement data preprocessing, model training, and saving functionality

--- Diabetes_prediction_deployed/Diabetes_Predictor.py
+++ Diabetes_prediction_deployed/Diabetes_Predictor.py
@@ -1,36 +1,72 @@
-# Importing essential libraries
+import pickle
 import numpy as np
 import pandas as pd
-import pickle
-
-# Loading the dataset
-df = pd.read_csv('dataset/diabetes.csv')
-
-# Renaming DiabetesPedigreeFunction as DPF
-df = df.rename(columns={'DiabetesPedigreeFunction':'DPF'})
-
-# Replacing the 0 values from ['Glucose','BloodPressure','SkinThickness','Insulin','BMI'] by NaN
-df_copy = df.copy(deep=True)
-df_copy[['Glucose','BloodPressure','SkinThickness','Insulin','BMI']] = df_copy[['Glucose','BloodPressure','SkinThickness','Insulin','BMI']].replace(0,np.NaN)
-
-# Replacing NaN value by mean, median depending upon distribution
-df_copy['Glucose'].fillna(df_copy['Glucose'].mean(), inplace=True)
-df_copy['BloodPressure'].fillna(df_copy['BloodPressure'].mean(), inplace=True)
-df_copy['SkinThickness'].fillna(df_copy['SkinThickness'].median(), inplace=True)
-df_copy['Insulin'].fillna(df_copy['Insulin'].median(), inplace=True)
-df_copy['BMI'].fillna(df_copy['BMI'].median(), inplace=True)
-
-# Model Building
-from sklearn.model_selection import train_test_split
-X = df.drop(columns='Outcome')
-y = df['Outcome']
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=0)
-
-# Creating Random Forest Model
 from sklearn.ensemble import RandomForestClassifier
-classifier = RandomForestClassifier(n_estimators=20)
-classifier.fit(X_train, y_train)
+from sklearn.model_selection import train_test_split
 
-# Creating a pickle file for the classifier
-filename = 'diabetes_prediction_model.pkl'
-pickle.dump(classifier, open(filename, 'wb'))
+
+def load_and_preprocess_data():
+    df = pd.read_csv('dataset/diabetes.csv')
+
+    df = df.rename(columns={'DiabetesPedigreeFunction': 'DPF'})
+
+    return df
+
+
+def handle_missing_values(df):
+    df_copy = df.copy()
+
+    # Columns to process
+    cols_to_impute = ['Glucose', 'BloodPressure',
+                      'SkinThickness', 'Insulin', 'BMI']
+
+    # Replace 0 with NaN
+    df_copy[cols_to_impute] = df_copy[cols_to_impute].replace(0, np.nan)
+
+    # Define imputation strategies
+    impute_strategy = {
+        'Glucose': df_copy['Glucose'].mean(),
+        'BloodPressure': df_copy['BloodPressure'].mean(),
+        'SkinThickness': df_copy['SkinThickness'].median(),
+        'Insulin': df_copy['Insulin'].median(),
+        'BMI': df_copy['BMI'].median()
+    }
+
+    # Apply imputation
+    for col, value in impute_strategy.items():
+        df_copy[col] = df_copy[col].fillna(value)
+
+    return df_copy
+
+
+def train_model(X, y):
+    X_train, _, y_train, _ = train_test_split(
+        X, y, test_size=0.2, random_state=0
+    )
+    # Creating and training model
+    classifier = RandomForestClassifier(n_estimators=20, random_state=0)
+    classifier.fit(X_train, y_train)
+
+    return classifier
+
+
+def main():
+    df = load_and_preprocess_data()
+    df_processed = handle_missing_values(df)
+
+    # Prepare features and target
+    X = df_processed.drop(columns='Outcome')
+    y = df_processed['Outcome']
+
+    # Model training
+    model = train_model(X, y)
+
+    # Save model
+    with open('diabetes_prediction_model.pkl', 'wb') as f:
+        pickle.dump(model, f)
+
+    print("Model trained and saved successfully!")
+
+
+if __name__ == "__main__":
+    main()
